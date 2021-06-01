ffa98c0848533f1b01e1b4abe33539bb9a2b7e4b
jamesj995 <mrjam7@gmail.com>
2021-06-01 15:29:57 -0500

    final code commit

--- assets/scores.js
+++ assets/scores.js
@@ -4,19 +4,43 @@ var scoreBtn = document.getElementById("scoreSubmit");
 var formEl = document.getElementById("scoreForm");
 var contentEl = document.getElementById("content");
 var userEntry = document.getElementById("ints");
+var scoreListEl = document.getElementById("scoreList");
 var score = localStorage.getItem("score");
 
-showScore.textContent = score;
+var savedScores = [];
 
-var savedInts = userEntry.value;
-localStorage.setItem("initials", savedInts);
-savedScores = {
-  initials: localStorage.getItem("initials"),
-  score: localStorage.getItem("score"),
-};
-localStorage.setItem("savedScores", JSON.stringify(savedScores));
+function renderScores() {
+  scoreListEl.innerHTML = "";
 
-function renderScores() {}
+  // Render a new li for each score
+  for (var i = 0; i < savedScores.length; i++) {
+    var scoreSS = savedScores[i];
+
+    var li = document.createElement("li");
+    li.textContent = scoreSS;
+    li.setAttribute("data-index", i);
+
+    scoreListEl.appendChild(li);
+  }
+}
+
+function init() {
+  scoreListEl.textContent = "";
+  // Get stored scores from localStorage
+  var storedScores = JSON.parse(localStorage.getItem("scores"));
+
+  // If scores were retrieved from localStorage, update the savedScores array to it
+  if (storedScores !== null) {
+    savedScores = storedScores;
+  }
+
+  // This is a helper function that will render scores to the DOM
+}
+
+function storeScores() {
+  // Stringify and set key in localStorage to savedScores array
+  localStorage.setItem("scores", JSON.stringify(savedScores));
+}
 
 rtBtn.addEventListener("click", function () {
   window.location.href = "index.html";
@@ -24,5 +48,22 @@ rtBtn.addEventListener("click", function () {
 
 scoreBtn.addEventListener("click", function () {
   contentEl.textContent = "";
+
+  var scoreInput = userEntry.value + ": " + score;
+
+  // Return from function early if submitted todoText is blank
+  if (scoreInput === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  savedScores.push(scoreInput);
+  userEntry.value = "";
+  console.log(savedScores);
+  // Store updated todos in localStorage, re-render the list
+  storeScores();
   renderScores();
 });
+
+showScore.textContent = score;
+init();
